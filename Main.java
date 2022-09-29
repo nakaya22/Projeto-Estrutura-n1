e367e6dce78ec7418eb17b6ce7d6328f30c99403
FernandesNz <91279997+FernandesNz@users.noreply.github.com>
2022-09-29 14:28:22 -0300

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,71 @@
+package lab;
+
+public class Main {
+	
+	public static void main(String[] args) {
+		
+		TadFila radio = new TadFila(10);
+	
+		//==============================================================
+		//Artistas
+		
+		Artista Matue = new Artista("Matue", "Ceara", "28");
+		Artista Teto = new Artista("Teto", "Bahia", "20");
+		Artista Wiu = new Artista("WIU", "Ceara", "19");
+		Artista Dfideliz = new Artista("Dfideliz", "São Paulo", "23");
+		Artista YunkVino = new Artista("Yunk Vino", "São Paulo", "25");
+		Artista Derek = new Artista("Derek", "São Paulo", "22");
+		Artista KayBlack = new Artista("kayBlack", "São Paulo", "21");
+		Artista JeSant = new Artista("Jé Santiago", "São Paulo", "25");
+		Artista YungBuda = new Artista("Yung Buda", "São Paulo", "24");
+		Artista Travis = new Artista("Travis Scott", "Texas", "31");
+		
+		
+		//===============================================================
+		//Musicas
+		
+		
+		Musica Vampiro = new Musica("Vampiro", "2022", Matue );
+		Slot s1 = new Slot(1,1, Vampiro);
+		
+		Musica SemDo = new Musica("Sem Do", "2021", Matue );
+		Slot s2 = new Slot(2,2, SemDo);
+		
+		Musica Groupies = new Musica("Groupies", "2021", Matue );
+		Slot s3 = new Slot(3,3, Groupies);
+			
+		Musica QuerVoar = new Musica("Quer Voar", "2021", Matue );
+		Slot s4 = new Slot(4,4, QuerVoar);
+	
+		Musica M4 = new Musica("M4", "2021", Matue );
+		Slot s5 = new Slot(5,5, M4);
+		
+		
+		//================================================================
+		
+		
+		radio.inserir(s1);
+		radio.inserir(s2);
+		radio.inserir(s3);
+		radio.inserir(s4);
+		radio.inserir(s5);
+		
+		
+		//================================================================
+		
+		System.out.println(s1.dscrição());
+		System.out.println(s2.dscrição());
+		System.out.println(s3.dscrição());
+		System.out.println(s4.dscrição());
+		System.out.println(s5.dscrição());
+		
+		System.out.println("===========");
+		
+		
+		radio.buscarDia(1);
+		
+		
+	
+		
+	}
+}
